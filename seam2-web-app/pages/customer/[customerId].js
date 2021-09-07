46d810d65d54d0963268de5bccf4207f6e45290c
Philip Lassen <philiplassen+git@gmail.com>
2021-09-07 05:49:27 -0700

    Fix table rendering

--- seam2-web-app/pages/customer/[customerId].js
+++ seam2-web-app/pages/customer/[customerId].js
@@ -6,24 +6,17 @@ const fetcher = (url) => fetch(url).then((res) => res.json())
 export default function Customer() {
   const { query } = useRouter()
   var customerId = query.customerId;
-  console.log("customerID", customerId);
-  var { data, error } = useSWR(`http://localhost:3000/api/devices/${customerId}`, fetcher);
-  console.log("What?");
-  console.log("data", data);
-  console.log("error", error);
-  //var response = await fetch(`http://localhost:3000/api/devices/${customerId}`);
-  //console.log(devices);
-
-
-  //return (<div></div>);
-  return renderTableData(data);;
-  //return renderTableData(data);
+  var { data, error } = useSWR(customerId === undefined ? null : `http://localhost:3000/api/devices/${customerId}`, fetcher);
+  if (error) return <div>{error.message}</div>
+  if (!data) return <div>Loading...</div>
+  return renderTableData(data);
 } 
 
 
 
 function renderTableData(rows) {
-  return rows.map(row => {
+  var cmp =
+  rows.map(row => {
      const { id, productId, deviceName} = row //destructuring
      return (
         <tr key={id}>
@@ -32,5 +25,12 @@ function renderTableData(rows) {
            <td>{deviceName}</td>
         </tr>
      )
-  })
+  });
+  return <>
+    {cmp}
+      <button type="button" onClick={ () => {
+        }}>
+      Click me
+    </button>
+    </>
 }
